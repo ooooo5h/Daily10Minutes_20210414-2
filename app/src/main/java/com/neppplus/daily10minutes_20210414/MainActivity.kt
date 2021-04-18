afa27acdd1f77a3f29b95264dc68275621d72c9c
전은형 <ooooo5h91@gmail.com>
2021-04-18 11:40:41 +0900

    로그아웃시 토큰 삭제 처리

--- app/src/main/java/com/neppplus/daily10minutes_20210414/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_20210414/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.neppplus.daily10minutes_20210414
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import com.neppplus.daily10minutes_20210414.adapters.ProjectAdapter
 import com.neppplus.daily10minutes_20210414.datas.Project
+import com.neppplus.daily10minutes_20210414.utils.ContextUtil
 import com.neppplus.daily10minutes_20210414.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -24,19 +26,26 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-
         logoutBtn.setOnClickListener {
 
             val alert = AlertDialog.Builder(mContext)
             alert.setTitle("로그아웃")
-            alert.setMessage("정말 로그아웃 하시겠습니까?")
-            alert.setPositiveButton("확인" , DialogInterface.OnClickListener { dialogInterface, i ->
+            alert.setMessage("정말 로그아웃하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
 
+                ContextUtil.setLoginToken(mContext, "")
+
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+
+                finish()
             })
-            alert.setNegativeButton("취소", null)
+            alert.setPositiveButton("취소", null)
             alert.show()
 
         }
+
+
 
     }
 
