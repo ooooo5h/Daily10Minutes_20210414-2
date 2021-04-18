80db7e0a96cbcc2ea6732af44d36c54949a60624
전은형 <ooooo5h91@gmail.com>
2021-04-18 16:41:20 +0900

    프로젝트 파싱시 태그 목록도 String목록으로 파싱했다

--- app/src/main/java/com/neppplus/daily10minutes_20210414/datas/Project.kt
+++ app/src/main/java/com/neppplus/daily10minutes_20210414/datas/Project.kt
@@ -9,7 +9,9 @@ class Project(
     var imageUrl : String,
     var description : String,
     var onGoingUserCount : Int,
-    var proofMethod : String) : Serializable{
+    var proofMethod : String) : Serializable {
+
+    val tags = ArrayList<String>()
 
     constructor() : this(0,"", "", "", 0, "")
 
@@ -26,6 +28,16 @@ class Project(
             project.onGoingUserCount = jsonObj.getInt("ongoing_users_count")
 
             project.proofMethod = jsonObj.getString("proof_method")
+
+            val tagsArr = jsonObj.getJSONArray("tags")
+
+            for (index in 0 until tagsArr.length()) {
+
+                val tagObj = tagsArr.getJSONObject(index)
+                val title = tagObj.getString("title")
+
+                project.tags.add(title)
+            }
 
             return project
         }
